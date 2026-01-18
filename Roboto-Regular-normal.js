66b5e65a07e468b44cbbdc3bd64003135e523ba4
kostia536 <84472905+kostia536@users.noreply.github.com>
2026-01-18 21:26:13 +0200

    Update Roboto-Regular-normal.js

--- Roboto-Regular-normal.js
+++ Roboto-Regular-normal.js
@@ -5,4 +5,4 @@ this.addFileToVFS("Roboto-Regular-normal.ttf", font);
 this.addFont("Roboto-Regular-normal.ttf", "Roboto-Regular", "normal");
 };
 jsPDFAPI.events.push(['addFonts', callAddFont])
- })(jsPDF.API);
+ })(window.jspdf.jsPDF.API);
